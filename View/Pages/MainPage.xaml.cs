88b8950c49138d3b0e2fabcbf9729f5f16dec0b6
Egor00001 <132343359+Egor00001@users.noreply.github.com>
2023-05-02 23:38:41 +0300

    Add files via upload

---
+++ View/Pages/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using курсач.Core;
+
+namespace курсач
+
+{
+    public partial class MainPage: Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTask_Click( object sender, RoutedEventArgs e )
+        {
+            ConnectionCore.CoreFrame?.Navigate(new TaskPage());
+        }
+
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
